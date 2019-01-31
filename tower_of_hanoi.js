070a1053471d9acdf3b08c6bc938ddeea8ac2686
Connel Allison <connelallison@hotmail.com>
2019-01-31 15:25:35 +0000

    refactors - removes the Puzzle object. the algorithm is now a single function (not a constructor) that can be called without creating a useless variable

--- tower_of_hanoi.js
+++ tower_of_hanoi.js
@@ -1,46 +1,41 @@
 "use strict";
 
-const Puzzle = function (height) {
-  this.moves = 0;
-  this.rings = this.constructPuzzle(height);
-  console.log(this.rings);
-  this.moveRing(0, 3);
-  console.log(this.rings);
-  console.log(`Moves: ${this.moves}`);
-}
+const towerOfHanoi = function (height) {
+  const moveRing = function (ring, target) {
+    const home = rings[ring];
+    const spare = [1, 2, 3].find((tower) => {
+      return tower !== target && tower !== home;
+    });
+    if (rings[ring + 1]) {
+        if (rings[ring + 1] !== spare) {
+          moveRing((ring + 1), spare);
+        }
+    }
+    rings[ring] = target;
+    console.log(`Moved ring ${ring} from ${home} to ${target}.`);
+    moves++;
+    if (rings[ring + 1]) {
+      moveRing((ring + 1), target);
+    }
+  }
 
-Puzzle.prototype.constructPuzzle = function (height) {
+  let moves = 0;
   const rings = [];
   for (let i = 0; i < height; i++) {
     rings.push(1);
-  }
-  return rings;
+  };
+  console.log(rings);
+  moveRing(0, 3);
+  console.log(rings);
+  console.log(`Moves: ${moves}`);
 }
 
-Puzzle.prototype.moveRing = function (ring, target) {
-  const home = this.rings[ring];
-  const spare = [1, 2, 3].find((tower) => {
-    return tower !== target && tower !== home;
-  });
-  if (this.rings[ring + 1]) {
-      if (this.rings[ring + 1] !== spare) {
-        this.moveRing((ring + 1), spare);
-      }
-  }
-  this.rings[ring] = target;
-  console.log(`Moved ring ${ring} from ${home} to ${target}.`);
-  this.moves++;
-  if (this.rings[ring + 1]) {
-    this.moveRing((ring + 1), target);
-  }
-}
-
-// const puzzle3 = new Puzzle(3);
-// const puzzle4 = new Puzzle(4);
-// const puzzle5 = new Puzzle(5);
-// const puzzle6 = new Puzzle(6);
-// const puzzle7 = new Puzzle(7);
-const puzzle8 = new Puzzle(8);
-// const puzzle9 = new Puzzle(9);
-// const puzzle10 = new Puzzle(10);
-// const puzzle11 = new Puzzle(11);
+// towerOfHanoi(3);
+// towerOfHanoi(4);
+// towerOfHanoi(5);
+// towerOfHanoi(6);
+// towerOfHanoi(7);
+towerOfHanoi(8);
+// towerOfHanoi(9);
+// towerOfHanoi(10);
+// towerOfHanoi(11);
