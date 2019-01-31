00f2feb319f4e967d4062886e4c37ec9c2c52315
Connel Allison <connelallison@hotmail.com>
2019-01-31 15:17:33 +0000

    refactoring - removed the Ring objects, is now just an array of integers where the index is the level and the integer is the tower

--- tower_of_hanoi.js
+++ tower_of_hanoi.js
@@ -1,49 +1,37 @@
 "use strict";
 
-const Ring = function (level) {
-  this.level = level;
-  this.tower = 1;
-}
-
 const Puzzle = function (height) {
   this.moves = 0;
   this.rings = this.constructPuzzle(height);
-  console.log(this.rings.map((ring) => {
-    return ring.tower;
-  }));
-  this.moveRing(this.rings[0], 3);
-  console.log(this.rings.map((ring) => {
-    return ring.tower;
-  }));
+  console.log(this.rings);
+  this.moveRing(0, 3);
+  console.log(this.rings);
   console.log(`Moves: ${this.moves}`);
 }
 
 Puzzle.prototype.constructPuzzle = function (height) {
   const rings = [];
   for (let i = 0; i < height; i++) {
-    const ring = new Ring(i);
-    rings.push(ring);
+    rings.push(1);
   }
   return rings;
 }
 
 Puzzle.prototype.moveRing = function (ring, target) {
-  const home = ring.tower;
+  const home = this.rings[ring];
   const spare = [1, 2, 3].find((tower) => {
     return tower !== target && tower !== home;
   });
-  if (this.rings[ring.level + 1]) {
-    // for (let i = ring.level + 1; i < array.length; i++) {
-      if (this.rings[ring.level + 1].tower !== spare) {
-        this.moveRing(this.rings[ring.level + 1], spare);
+  if (this.rings[ring + 1]) {
+      if (this.rings[ring + 1] !== spare) {
+        this.moveRing((ring + 1), spare);
       }
-    // }
   }
-  ring.tower = target;
-  console.log(`Moved ring ${ring.level} from ${home} to ${target}.`);
+  this.rings[ring] = target;
+  console.log(`Moved ring ${ring} from ${home} to ${target}.`);
   this.moves++;
-  if (this.rings[ring.level + 1]) {
-    this.moveRing(this.rings[ring.level + 1], target);
+  if (this.rings[ring + 1]) {
+    this.moveRing((ring + 1), target);
   }
 }
 
@@ -53,3 +41,6 @@ Puzzle.prototype.moveRing = function (ring, target) {
 // const puzzle6 = new Puzzle(6);
 // const puzzle7 = new Puzzle(7);
 const puzzle8 = new Puzzle(8);
+// const puzzle9 = new Puzzle(9);
+// const puzzle10 = new Puzzle(10);
+// const puzzle11 = new Puzzle(11);
