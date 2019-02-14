c6bff3c4cf99cccec450664f5bb279526f16e0e4
Connel Allison <connelallison@hotmail.com>
2019-02-14 16:28:19 +0000

    further optimises the no_lambda version, reducing time by about half. also adds option to toggle the moves counter - when running the program, add any additional argument after the number of rings to enable it.

--- tower_of_hanoi_no_lambda.cpp
+++ tower_of_hanoi_no_lambda.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-void moveRing(int rings[], int& moves, int ringIndex, int target);
+void moveRing(int rings[], long& moves, int ringIndex, int target);
+void moveRing(int rings[], int ringIndex, int target);
 
 int main(int argc, char const *argv[]) {
     using namespace std;
@@ -12,8 +13,8 @@ int main(int argc, char const *argv[]) {
    } else {
      heightInput = 12;
    }
+
     // cout << heightInput << endl;
-    int moves = 0;
     int rings[heightInput + 1];
     for (int i = 0; i < heightInput; i++) {
         rings[i] = 1;
@@ -23,25 +24,32 @@ int main(int argc, char const *argv[]) {
     //     cout << rings[i] << " ";
     // }
     // cout << endl;
-    moveRing(rings, moves, 0, 3);
+    if (argc > 2) {
+      long moves = 0;
+      moveRing(rings, moves, 0, 3);
+      cout << "Moves: " << moves << endl;
+    } else {
+      moveRing(rings, 0, 3);
+    }
+
     // for (int i = 0; i < heightInput; i++) {
     //     cout << rings[i] << " ";
     // }
     // cout << endl;
-    // cout << "Moves: " << moves << endl;
 
     return 0;
 }
 
-void moveRing(int rings[], int& moves, int ringIndex, int target){
+void moveRing(int rings[], long& moves, int ringIndex, int target){
   int home = rings[ringIndex];
-  int spare = 0;
-  for (int i = 1; i < 4; i++) {
-      if (i != target && i != home) {
-          spare = i;
-      }
-  }
-  if (rings[ringIndex + 1] != 0) {
+  int spare = 6 - target - home;
+  // int spare;
+  // for (int i = 1; i < 4; i++) {
+  //     if (i != target && i != home) {
+  //         spare = i;
+  //     }
+  // }
+  if (rings[ringIndex + 1]) {
       if (rings[ringIndex + 1] != spare) {
           moveRing(rings, moves, (ringIndex + 1), spare);
       }
@@ -51,5 +59,26 @@ void moveRing(int rings[], int& moves, int ringIndex, int target){
   moves++;
   if (rings[ringIndex + 1]) {
       moveRing(rings, moves, (ringIndex + 1), target);
+  }
+}
+
+void moveRing(int rings[], int ringIndex, int target){
+  int home = rings[ringIndex];
+  int spare = 6 - target - home;
+  // int spare;
+  // for (int i = 1; i < 4; i++) {
+  //     if (i != target && i != home) {
+  //         spare = i;
+  //     }
+  // }
+  if (rings[ringIndex + 1]) {
+      if (rings[ringIndex + 1] != spare) {
+          moveRing(rings, (ringIndex + 1), spare);
+      }
+  }
+  rings[ringIndex] = target;
+  // cout << "Moved ring " << ringIndex << " from " << home << " to " << target << "." << endl;
+  if (rings[ringIndex + 1]) {
+      moveRing(rings, (ringIndex + 1), target);
   }
 }
